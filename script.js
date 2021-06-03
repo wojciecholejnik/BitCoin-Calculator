9be50d28a611000039b600c8f2851a332f59ae84
Wojtek <68703185+wojciecholejnik@users.noreply.github.com>
2021-06-03 19:17:34 +0200

    second version

--- script.js
+++ script.js
@@ -8,20 +8,19 @@ const BTCInput = document.getElementById('BTC-input');
   .then(data => {
 
     const PLN = data.btc_pln;
-    const BTC = data.ltc_btc;
 
-    console.log(PLN);
-    console.log(BTC);
+    const PLNChange = parseFloat(PLN.change.slice(0,PLN.change.length-1)) * 0.01;
 
-    rate.innerHTML = PLN.bid;
+    rate.innerHTML = (PLN.ask * (1 + PLNChange)).toFixed(2);
 
-    BTCInput.value = (PLNInput.value / PLN.bid).toFixed(8);
+    // calculate BTC default value with fetched rate
+    BTCInput.value = (PLNInput.value / PLN.ask).toFixed(8);
 
+    // set onchange to inputs
     PLNInput.onchange = () => {
-      BTCInput.value = (PLNInput.value / PLN.bid).toFixed(8);
+      BTCInput.value = (PLNInput.value / (PLN.ask * (1 + PLNChange))).toFixed(8);
     };
-
     BTCInput.onchange = () => {
-      PLNInput.value = (BTCInput.value * PLN.bid).toFixed(8);
+      PLNInput.value = (BTCInput.value * PLN.ask).toFixed(2);
     }
   });
